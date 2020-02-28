14f6407263bd3fe05c07c98cd2cea5f33e404c02
Juliano <contato.julianoalvess@gmail.com>
2020-02-28 12:43:22 -0300

    Corpo parser

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.post('/sms', (req, res) => {
     const corpo = req.body.Body;
     // const status = req.body.MessageSid;
 
-    message.body(`Sua resposta é ${corpo}`)
+    message.body(`Sua resposta é ${JSON.stringify(corpo)}`)
 
     console.log(response)
 
