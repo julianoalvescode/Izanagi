41b05dcd6d1bdc95a151ded2269071c5ff99394d
Juliano <contato.julianoalvess@gmail.com>
2020-02-28 10:19:08 -0300

    4

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ app.listen(port, function () {
 });
 
 app.get('/', (req, res) => {
-  client.messages
-  .create({
-     mediaUrl: ['https://avatars2.githubusercontent.com/u/43914533?s=460&v=4'],
-     from: 'whatsapp:+14155238886',
-     body: `Este é você`,
-     to: 'whatsapp:+5521969023070'
-   })
-  .then(message => console.log(message.sid));
+  // client.messages
+  // .create({
+  //    mediaUrl: ['https://avatars2.githubusercontent.com/u/43914533?s=460&v=4'],
+  //    from: 'whatsapp:+14155238886',
+  //    body: `Este é você`,
+  //    to: 'whatsapp:+5521969023070'
+  //  })
+  // .then(message => console.log(message.sid));
       res.send('<h1>Testando Tudo</h1>')
 })
 
