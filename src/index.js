2e2a2b67b215335e34596196ef66eaeef3d112ef
Juliano <contato.julianoalvess@gmail.com>
2020-02-28 09:55:49 -0300

    Bot

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const assistant = require('./service/api-watson')
+const client = require('./service/api-twilio')
+const axios = require('axios').default;
+
+
+const text = 'Ainda bebe, rôbo do Juliano'
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
+
+app.get('/', (req, res) => {
+  client.messages
+  .create({
+     mediaUrl: ['https://avatars2.githubusercontent.com/u/43914533?s=460&v=4'],
+     from: 'whatsapp:+14155238886',
+     body: `Este é você`,
+     to: 'whatsapp:+5521969023070'
+   })
+  .then(message => console.log(message.sid));
+      res.send('<h1>Testando Tudo</h1>')
+})
+
