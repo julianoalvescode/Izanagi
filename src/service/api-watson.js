955152c9207ddbab54769e0365734315e9e7e133
Juliano Alves <contato.julianoalvess@gmail.com>
2020-04-10 14:39:57 -0300

    Update api-watson.js

--- src/service/api-watson.js
+++ src/service/api-watson.js
@@ -5,9 +5,9 @@ const { IamAuthenticator } = require('ibm-watson/auth')
 const assistant = new AssistantV2({
   version: '2020-02-05',
   authenticator: new IamAuthenticator({
-    apikey: 'OjOd9KDEku2VSvd7t-M62SF3Ms1KRcLiBLoHgpH0nSEC'
+    apikey: ''
   }),
-  url: 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/40bdabd3-8ff9-42cb-aeba-f71801d67ebc',
+  url: '',
   disableSslVerification: true
 })
 
