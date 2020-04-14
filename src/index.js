2f8f461a6dd08f908e33f299a9fcdd8fa701cab9
Juliano Alves <contato.julianoalvess@gmail.com>
2020-04-13 21:58:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Routes = require('./routes/Routes')
 
 var port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Umbler listening on port %s', port)
+  console.log('Server on %s', port)
 })
 
 mongoose.connect('',
